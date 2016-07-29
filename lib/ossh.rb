6116e9e7946f670370ddf83dcd0e90294fe59203
Kirill Timofeev <kirill.timofeev@hulu.com>
2016-07-28 21:31:40 -0700

    big refactoring: MultiSSH -> OSSHDispatcher + OSSHHost

--- lib/ossh.rb
+++ lib/ossh.rb
@@ -27,32 +27,29 @@ HOST_SUFFIX = {
     :error => "[!]"
 }
 
-class MultiSSH
-    def initialize(hosts, options)
-        @options = options
-        @host_index = 0
-        @hosts_done = 0
-        @hosts_failed = 0
-        @pool = []
-        @auth_methods = ["publickey"]
-        max_host_length = hosts.map {|h| h[:label].length}.max
-        if options[:password]
-            @auth_methods << "password"
-        end
-        hosts.sort {|x, y| x[:address] <=> y[:address]}.each do |host|
-            host[:label] += (" " * (max_host_length - host[:label].length))
-            @pool << host
-        end
-        if options[:preconnect]
-            preconnect()
-        else
-            exec()
-        end
+class OSSHHost
+    def initialize(address, label, dispatcher, options)
+        @address = address
+        @label = label
+        @dispatcher = dispatcher
+        @ssh = nil
+        @error = nil
+        @exit_code = nil
+        @timer = nil
+        @username = options[:username]
+        @password = options[:password]
+        @auth_methods = options[:auth_methods]
+        @command = options[:command]
+        @timeout = options[:timeout]
+        @buffer = {
+            :stdout => "",
+            :stderr => ""
+        }
     end
 
-    def start_ssh(host)
-        EM::Ssh.start(host[:address], @options[:username],
-                :password => @options[:password],
+    def start_ssh()
+        EM::Ssh.start(@address, @username,
+                :password => @password,
                 :timeout => SSH_TIMEOUT,
                 :global_known_hosts_file => "/dev/null",
                 :user_known_hosts_file => "/dev/null",
@@ -64,94 +61,31 @@ class MultiSSH
         end
     end
 
-    def preconnect()
-        @pool.each do |host|
-            start_ssh(host) do |connection|
-                connection.errback do |err|
-                    host[:error] = "#{err} (#{err.class})"
-                    host_processed(host)
-                end
-                connection.callback do |ssh|
-                    host[:ssh] = ssh
-                    host_processed(host)
-                end
-            end
-        end
-    end 
-
-    def host_prefix(host, out_type)
+    def prefix(out_type)
         if USE_COLOR
-            return HIGHLINE.color(host, HOST_COLOR[out_type])
+            return HIGHLINE.color(@label, HOST_COLOR[out_type])
         else
-            return "#{host} #{HOST_SUFFIX[out_type]}"
+            return "#{@label} #{HOST_SUFFIX[out_type]}"
         end
     end
 
-    def host_processed(host)
-        @hosts_done += 1
-        if host[:error] != nil
-            print "#{host_prefix(host[:label], :error)} #{host[:error]}\n"
-            @hosts_failed += 1
-        end
-        if @hosts_done == @pool.size()
-            if @hosts_failed > 0 && ! @options[:ignore_failures]
-                EM.stop()
-                raise OSSHException.new("Failed to connect to #{@hosts_failed} hosts, exiting")
-            end
-            @pool.select! {|x| x[:ssh] }
-            exec()
-        end
-    end
-
-    def exec()
-        @hosts_done = 0
-        [@pool.size(), @options[:concurrency]].min.times do
-            exec_next()
-        end
-    end
-
-    def process_output(host, out_type, data)
+    def process_output(out_type, data)
         # stdout is usually flushed by complete lines, but not stderr, which is flushed immediately
         # to have reasonable output we need to buffer output and print it when we are sure we 
         # have complete line (terminating newline is present)
-        host[out_type] += data
-        if host[out_type].include?("\n")
-            out = host[out_type].split("\n")
-            if host[out_type].end_with?("\n")
-                host[out_type] = ""
+        @buffer[out_type] += data
+        if @buffer[out_type].include?("\n")
+            out = @buffer[out_type].split("\n")
+            if @buffer[out_type].end_with?("\n")
+                @buffer[out_type] = ""
             else
-                host[out_type] = out.pop
+                @buffer[out_type] = out.pop
             end
-            print out.map {|x| "#{host_prefix(host[:label], out_type)} #{x}" }.join("\n") + "\n"
+            print out.map {|x| "#{prefix(out_type)} #{x}" }.join("\n") + "\n"
         end
     end
 
-    def exec_next()
-        if @hosts_done == @pool.size()
-            EM.stop()
-            return
-        end
-        host = @pool[@host_index]
-        return if host.nil?
-        @host_index += 1
-        if host[:ssh]
-            exec_single(host)
-        else
-            start_ssh(host) do |connection|
-                connection.errback do |err|
-                    @hosts_done += 1
-                    print "#{host_prefix(host[:label], :error)} #{err} (#{err.class})\n"
-                    exec_next()
-                end
-                connection.callback do |ssh|
-                    host[:ssh] = ssh
-                    exec_single(host)
-                end
-            end
-        end
-    end
-
-    def finalize_connection(ch, host)
+    def finalize_connection(ch)
         # without explicit connection close socket would stay opened and we may run out of file descriptors
         # Fiber is needed to avoid "can't yield from root fiber" error
         Fiber.new {
@@ -160,58 +94,150 @@ class MultiSSH
             rescue
             end
         }.resume
-        @hosts_done += 1
         # let's check if we have any buffered output that wasn't printed yet
         [:stdout, :stderr].each do |out_type|
-            next if host[out_type].empty?
+            next if @buffer[out_type].empty?
             data = "\n"
-            if host[out_type].end_with?("\n")
+            if @buffer[out_type].end_with?("\n")
                 data = ""
             end
-            process_output(host, out_type, data)
+            process_output(out_type, data)
         end
-        print "#{host_prefix(host[:label], :error)} #{host[:error]}\n" if host[:error]
-        exec_next()
+        # @error may be set in case of timeout
+        print "#{prefix(:error)} #{@error}\n" if @error
+        @dispatcher.continue_run()
     end
 
-    def exec_single(host)
-        host[:exit_code] = nil
-        host[:stdout] = ""
-        host[:stderr] = ""
-        channel = host[:ssh].open_channel do |oc|
+    def do_run()
+        channel = @ssh.open_channel do |oc|
             # with request_pty stdout and stderr are combined
             # since pty is needed only for interactive programs let's try without it
             #oc.request_pty
-            oc.exec(@options[:command]) do |ch, success|
+            oc.exec(@command) do |ch, success|
                 if success
-                    if @options[:timeout] > 0
-                        host[:timer] = EM::Timer.new(@options[:timeout]) do
-                            host[:error] = "terminated on timeout"
-                            finalize_connection(ch, host)
+                    if @timeout > 0
+                        @timer = EM::Timer.new(@timeout) do
+                            @error = "terminated on timeout"
+                            finalize_connection(ch)
                         end
                     end
                     channel.on_data do |ch, data|
-                        process_output(host, :stdout, data)
+                        process_output(:stdout, data)
                     end
                     channel.on_extended_data do |ch, type, data|
-                        process_output(host, :stderr, data)
+                        process_output(:stderr, data)
                     end
                     channel.on_request("exit-status") do |ch, data|
                         # IMPORTANT!!! On some platforms exit-status is received BEFORE final output data
-                        host[:exit_code] = data.read_long
+                        @exit_code = data.read_long
                     end
                     channel.on_close do |ch|
-                        host[:timer].cancel if host[:timer]
-                        finalize_connection(ch, host)
+                        @timer.cancel if @timer
+                        finalize_connection(ch)
                     end
                 else
-                    print "#{host_prefix(host[:label], :error)} exec() failed\n"
-                    @hosts_done += 1
-                    exec_next()
+                    print "#{prefix(:error)} exec() failed\n"
+                    @dispatcher.continue_run()
                 end
             end
         end
-    end 
+    end
+
+    def preconnect()
+        start_ssh() do |connection|
+            connection.errback  {|err| preconnect_cb(nil, err) }
+            connection.callback {|ssh| preconnect_cb(ssh, nil) }
+        end
+    end
+
+    def preconnect_cb(ssh, err)
+        print "#{prefix(:error)} #{err} (#{err.class})\n" if err
+        @ssh = ssh
+        @dispatcher.preconnect_done(ssh)
+    end
+
+    def connected?
+        @ssh != nil
+    end
+
+    def run()
+        if @ssh
+            do_run()
+        else
+            start_ssh() do |connection|
+                connection.errback do |err|
+                    print "#{prefix(:error)} #{err} (#{err.class})\n"
+                    @dispatcher.continue_run()
+                end
+                connection.callback do |ssh|
+                    @ssh = ssh
+                    do_run()
+                end
+            end
+        end
+    end
+end
+
+class OSSHDispatcher
+    def initialize(hosts, options)
+        @host_index = 0
+        @hosts_done = 0
+        @hosts_failed = 0
+        @all_hosts = []
+        @preconnect = options[:preconnect]
+        @ignore_failures = options[:ignore_failures]
+        @concurrency = options[:concurrency]
+        options[:auth_methods] = ["publickey"]
+        options[:auth_methods] << "password" if options[:password]
+        max_host_length = hosts.map {|h| h[:label].length}.max
+        hosts.sort {|x, y| x[:address] <=> y[:address]}.each do |h|
+            @all_hosts << OSSHHost.new(h[:address], h[:label].ljust(max_host_length), self, options)
+        end
+    end
+
+    def run()
+        if @preconnect
+            @all_hosts.each {|h| h.preconnect()}
+        else
+            start_run()
+        end
+    end
+
+    def preconnect_done(ssh)
+        @hosts_done += 1
+        @hosts_failed += 1 if ssh.nil?
+        if @hosts_done == @all_hosts.size()
+            if @hosts_failed > 0 && ! @ignore_failures
+                EM.stop()
+                raise OSSHException.new("Failed to connect to #{@hosts_failed} hosts, exiting")
+            end
+            @all_hosts.select! {|x| x.connected? }
+            start_run()
+        end
+    end
+
+    def continue_run()
+        @hosts_done += 1
+        run_next()
+    end
+
+    def start_run()
+        @hosts_done = 0
+        [@all_hosts.size(), @concurrency].min.times do
+            run_next()
+        end
+    end
+
+    def run_next()
+        if @hosts_done == @all_hosts.size()
+            EM.stop()
+            return
+        end
+        host = @all_hosts[@host_index]
+        return if host.nil?
+        @host_index += 1
+        host.run()
+    end
 end
 
 class OSSHException < Exception
@@ -287,7 +313,7 @@ class OSSH
 
         EM.epoll
         EM.run do
-            MultiSSH.new(hosts, @options)
+            OSSHDispatcher.new(hosts, @options).run()
         end
     end
 end
