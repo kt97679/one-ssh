6a6ddff759da97beeab48c0eb784c336434406aa
Kirill Timofeev <kirill.timofeev@hulu.com>
2017-07-26 13:35:35 -0700

    added preliminary version of python implementation

---
+++ py/ossh.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+from braceexpand import braceexpand
+import asyncio
+import asyncssh
+import sys
+import importlib.machinery
+import types
+import getpass
+
+OSSH_INVENTORY = None
+
+try:
+    loader = importlib.machinery.SourceFileLoader('ossh_inventory', os.environ['HOME'] + '/.ossh.py')
+    mod = types.ModuleType(loader.name)
+    loader.exec_module(mod)
+    OSSH_INVENTORY = mod.OSSHInventory
+except Exception as e:
+    pass
+
+DEFAULT_CONCURRENCY = 256
+DEFAULT_SSH_CONNECTION_TIMEOUT = 60
+
+USE_COLOR = sys.stdout.isatty()
+
+COLOR_CODE = {
+    'CYAN': '\033[96m',
+    'YELLOW': '\033[93m',
+    'RED': '\033[91m',
+    'RESET_COLOR': '\033[0m'
+}
+
+# each output line is prefixed with host name
+# depending on output type host name would have different colors
+HOST_COLOR = {
+    'stdout': 'CYAN',
+    'stderr': 'YELLOW',
+    'error': 'RED'
+}
+
+# if we are not using color host name would have following suffixes depending on the output type
+HOST_SUFFIX = {
+    'stdout': "[1]",
+    'stderr': "[2]",
+    'error': "[!]"
+}
+
+options = {
+    'timeout': 0,
+    'connection_timeout': DEFAULT_SSH_CONNECTION_TIMEOUT,
+    'username': os.environ.get('LOGNAME'),
+    'concurrency': DEFAULT_CONCURRENCY,
+    'ignore_failures': False,
+    'resolve_ip': True,
+    'preconnect': False,
+    'host_file': [],
+    'host_string': [],
+    'inventory': [],
+    'keys': None,
+    'command': []
+}
+
+class MySSHClientSession(asyncssh.SSHClientSession):
+    def __init__(self, host):
+        self.host = host
+
+    def data_received(self, data, datatype):
+        self.host.process_data(data, 'stderr' if datatype == asyncssh.EXTENDED_DATA_STDERR else 'stdout')
+
+    def connection_lost(self, exc):
+        if exc:
+            print('SSH session error: ' + str(exc), file=sys.stderr)
+
+class MySSHClient(asyncssh.SSHClient):
+    def __init__(self, host):
+        self.host = host
+
+    def connection_made(self, conn):
+        print('Connection made to {} ({}).'.format(conn.get_extra_info('peername')[0], self.host.label))
+
+    def auth_completed(self):
+        print('Authentication successful.')
+
+class OSSHHost():
+    def __init__(self, addr, label):
+        self.addr = addr
+        self.label = label
+        self.buf = {
+            'stdout': '',
+            'stderr': ''
+        }
+
+    def process_data(self, data, datatype):
+        self.buf[datatype] += data
+        out = self.buf[datatype].split('\n')
+        self.buf[datatype] = out.pop()
+        color_code = COLOR_CODE[HOST_COLOR[datatype]]
+        for line in out:
+            print("{}{}{} {}".format(color_code, self.label, COLOR_CODE['RESET_COLOR'], line))
+
+class OSSH():
+    def get_client_factory(self, host):
+        def client_factory():
+            return MySSHClient(host)
+        return client_factory
+
+    def get_client_session_factory(self, host):
+        def client_factory():
+            return MySSHClientSession(host)
+        return client_factory
+
+    def run(self, args):
+        print(args)
+        self.args = args
+        self.hosts = []
+        hosts = []
+        if args.hosts_string:
+            for h in args.hosts_string:
+                hosts += list(braceexpand(h))
+        for h in hosts:
+            self.hosts.append(OSSHHost(h, h.split('.')[0]))
+        max_label_len = len(max([x.label for x in self.hosts], key=len))
+        for h in self.hosts:
+            h.label = h.label.ljust(max_label_len)
+        self.command = "\n".join(args.command)
+        self.dispatcher = self._dispatcher()
+        self.par = args.par
+        
+        self.loop = asyncio.get_event_loop()
+        asyncio.ensure_future(self.run_multiple_clients())
+        try:
+            self.loop.run_forever()
+        finally:
+            self.loop.close() 
+
+    async def run_client(self, host, command):
+        client_keys = ()
+        if self.args.key:
+            client_keys = self.args.key
+#        if self.args.password:
+#            client_keys = None
+        try:
+            conn, client = await asyncssh.create_connection(self.get_client_factory(host), host.addr, password=self.args.password, username=self.args.user, client_keys=client_keys)
+            async with conn:
+                chan, session = await conn.create_session(self.get_client_session_factory(host), command)
+                await chan.wait_closed()
+        except Exception as e:
+            print(e)
+        try:
+            next(self.dispatcher)
+        except StopIteration:
+            pass
+    
+    async def run_multiple_clients(self):
+        next(self.dispatcher)
+    
+    def _dispatcher(self):
+        running = 0
+        for h in self.hosts:
+            asyncio.ensure_future(self.run_client(h, self.command))
+            running += 1
+            if running < self.par:
+                continue
+            yield
+            running -= 1
+        for h in range(0, running):
+            yield
+        self.loop.stop()
+
+class OSSHCli(OSSH):
+    def parse_cli_args(self):
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument('-p', '--par', type=int, default=DEFAULT_CONCURRENCY, help="How many hosts to run simultaneously (default: %(default)d)")
+        parser.add_argument('-C', '--command-file', type=str, help="File with commands to run", action="append")
+        parser.add_argument('-c', '--command', type=str, help="Command to run", action="append")
+        parser.add_argument('-A', '--askpass', help="Prompt for a password for ssh connects (default: use key based authentication)", action="store_true")
+        parser.add_argument('-l', '--user', type=str, default=os.environ.get('LOGNAME'), help="Username for connections (default: $LOGNAME)")
+        parser.add_argument('-t', '--timeout', type=int, default=0, help="Timeout for operation, 0 for no timeout (default: %(default)d)")
+        parser.add_argument('-H', '--hosts-string', type=str, action="append", help="Add the given HOSTS_STRING to the list of hosts. "
+                        "HOSTS_STRING can contain multiple hosts separated by space, brace expansion can be used. "
+                        "E.g. \"host{1,3..5}.com\" would expand to \"host1.com host3.com host4.com host5.com\" "
+                        "This option can be used multiple times.")
+        parser.add_argument('-h', '--hosts-file', type=str, action="append", help="Read hosts from the given HOSTS_FILE. "
+                        "Each line in the HOSTS_FILE should be like HOSTS_STRING above. "
+                        "This option can be used multiple times.")
+        parser.add_argument("-n", "--noresolve", help="Don't resolve ip addresses to names", action="store_true")
+        parser.add_argument("-P", "--preconnect", help="Connect to all hosts before running command", action="store_true")
+        parser.add_argument('-i', '--ignore-failures', help="Ignore connection failures in the preconnect mode", action="store_false")
+        parser.add_argument('-k', '--key', type=str, action="append", help="Use this private key. This option can be used multiple times")
+        if OSSH_INVENTORY and OSSH_INVENTORY.get_inventory:
+            parser.add_argument("-I", "--inventory", type=str, action="append", help="Use INVENTORY expression to select hosts. "
+                            "This option can be used multiple times.")
+        parser.add_argument("-?", "--help", help="Show help", action="help")
+        args = parser.parse_args()
+        return args
+
+    def run(self):
+        args = self.parse_cli_args()
+        args.password = None
+        if args.askpass:
+            args.password = getpass.getpass('Password: ')
+
+        super(OSSHCli, self).run(args)
+
+if __name__ == '__main__':
+    OSSHCli().run()
