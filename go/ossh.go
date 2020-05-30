5f0c191b357ff12a01456a67869ff809f42d2998
Kirill Timofeev <kt97679@gmail.com>
2020-05-29 17:04:04 -0700

    OsshMessage moved to separate file

--- go/ossh.go
+++ go/ossh.go
@@ -25,24 +25,6 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-var useColor bool
-var verbose *bool
-
-const (
-	STDOUT  = 1
-	STDERR  = 2
-	EXIT    = 4
-	ERROR   = 8
-	STATUS  = 16
-	VERBOSE = 32
-)
-
-type OsshMessage struct {
-	data        string
-	messageType int
-	host        *OsshHost
-}
-
 func PublicKeyFile(file string) (ssh.AuthMethod, error) {
 	var key ssh.Signer
 	var err error
@@ -100,35 +82,6 @@ func get_ssh_client_config(logname *string, key *string, password string) (*ssh.
 		Auth:            auth_method,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}, nil
-}
-
-var hostColor = map[int]int{
-	ERROR:   31, // red
-	STDOUT:  36, // cyan
-	STDERR:  33, // yellow
-	VERBOSE: 32, //green
-}
-
-var hostSuffix = map[int]string{
-	ERROR:   "[!]",
-	STDOUT:  "[1]",
-	STDERR:  "[2]",
-	VERBOSE: "[v]",
-}
-
-func (m *OsshMessage) println() {
-	if m.messageType == VERBOSE && !*verbose {
-		return
-	}
-	out := m.data
-	if m.messageType == ERROR {
-		out = m.host.err.Error()
-	}
-	if useColor {
-		fmt.Printf("\033[%dm%s\033[0m %s\n", hostColor[m.messageType], m.host.label, out)
-	} else {
-		fmt.Printf("%s %s %s\n", m.host.label, hostSuffix[m.messageType], out)
-	}
 }
 
 // we need to define new type because by default getopt will split
---
+++ go/ossh_message.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+var useColor bool
+var verbose *bool
+
+const (
+	STDOUT  = 1
+	STDERR  = 2
+	EXIT    = 4
+	ERROR   = 8
+	STATUS  = 16
+	VERBOSE = 32
+)
+
+type OsshMessage struct {
+	data        string
+	messageType int
+	host        *OsshHost
+}
+
+var hostColor = map[int]int{
+	ERROR:   31, // red
+	STDOUT:  36, // cyan
+	STDERR:  33, // yellow
+	VERBOSE: 32, // green
+}
+
+var hostSuffix = map[int]string{
+	ERROR:   "[!]",
+	STDOUT:  "[1]",
+	STDERR:  "[2]",
+	VERBOSE: "[v]",
+}
+
+func (m *OsshMessage) println() {
+	if m.messageType == VERBOSE && !*verbose {
+		return
+	}
+	out := m.data
+	if m.messageType == ERROR {
+		out = m.host.err.Error()
+	}
+	if useColor {
+		fmt.Printf("\033[%dm%s\033[0m %s\n", hostColor[m.messageType], m.host.label, out)
+	} else {
+		fmt.Printf("%s %s %s\n", m.host.label, hostSuffix[m.messageType], out)
+	}
+}
