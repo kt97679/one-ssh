bbb6e5fd142b5d0ecbe98ac9f0e739feccd6da2b
Allison Maheu <allison.maheu@disneystreaming.com>
2021-03-25 14:13:10 -0400

    Refactor parsing for public keys

--- ossh_util.go
+++ ossh_util.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crypto/x509"
 	"encoding/pem"
 	"errors"
 	"fmt"
@@ -27,23 +26,21 @@ func publicKeyFile(file string) (ssh.AuthMethod, error) {
 	if err != nil {
 		return nil, err
 	}
-	p, rest := pem.Decode(buffer)
+	_, rest := pem.Decode(buffer)
 	if len(rest) > 0 {
 		return nil, errors.New("Failed to decode the key \"" + file + "\"")
 	}
-	pBlock := pem.Block{
-		Bytes:   buffer,
-		Type:    p.Type,
-		Headers: p.Headers,
-	}
-	if x509.IsEncryptedPEMBlock(&pBlock) {
-		key, err = ssh.ParsePrivateKeyWithPassphrase(buffer, readBytePasswordFromTerminal("SSH Passphrase:"))
-	} else {
-		key, err = ssh.ParsePrivateKey(buffer)
-	}
-	if err != nil {
+
+	if key, err = ssh.ParsePrivateKey(buffer); err != nil {
+		if err == err.(*ssh.PassphraseMissingError) {
+			if key, err = ssh.ParsePrivateKeyWithPassphrase(buffer, readBytePasswordFromTerminal("SSH Passphrase:")); err != nil {
+				return nil, err
+			}
+			return ssh.PublicKeys(key), nil
+		}
 		return nil, err
 	}
+
 	return ssh.PublicKeys(key), nil
 }
 
